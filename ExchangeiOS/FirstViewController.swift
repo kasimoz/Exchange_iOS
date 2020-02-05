076f0a3df73958b4a23c561232ef4672aff000af
kasimozdemir <kasim.ozdemir@icisleri.gov.tr>
2020-02-05 15:00:36 +0300

    Initial Commit

---
+++ ExchangeiOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ExchangeiOS
+//
+//  Created by KasimOzdemir on 5.02.2020.
+//  Copyright © 2020 KasimOzdemir. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExchangeiOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ExchangeiOS
+//
+//  Created by KasimOzdemir on 5.02.2020.
+//  Copyright © 2020 KasimOzdemir. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
